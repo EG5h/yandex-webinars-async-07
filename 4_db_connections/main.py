2c3a674147aeea0a035b688f208b129a3ee6c1ea
SlavaSkvortsov <29122694+SlavaSkvortsov@users.noreply.github.com>
2023-04-15 19:08:12 +0200

    Webinar 4 - comments

--- 4_db_connections/main.py
+++ 4_db_connections/main.py
@@ -11,13 +11,46 @@ from db import Session
 app = FastAPI()
 
 
+@app.get('/')
+def root() -> int:
+    """
+    Синхронный ендпоинт, максимум 40 параллельных запросов
+    """
+    time.sleep(1)
+    return 1
+
+
+@app.get('/')
+async def root() -> int:
+    """
+    Асинхронный ендпоинт, число параллельных запросов не ограничено (только железом)
+    """
+    await asyncio.sleep(1)
+    return 1
+
+
 async def get_session() -> AsyncSession:
     async with Session() as session:
         yield session
 
 
 @app.get('/')
+async def root(session: AsyncSession = Depends(get_session)) -> int:
+    """
+    Асинхронный ендпоинт, ограничено соединениями с БД. Сессия открывается
+    в начале запроса и закрывается в конце. Мы просто так держим сессию открытой когда происходит sleep
+    """
+    await session.execute(text('SELECT pg_sleep(1)'))
+    await asyncio.sleep(1)
+    return 1
+
+
+@app.get('/')
 async def root() -> int:
+    """
+    Асинхронный ендпоинт, ограничено соединениями с БД. Но при этом сессия создается
+    только когда нам это нужно, эффективно расходуя connection pool
+    """
     async with Session() as session:
         await session.execute(text('SELECT pg_sleep(1)'))
 
